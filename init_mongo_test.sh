ca7fe4945f637b4843c6b1b0f9b7f9df084e56bd
Cyril connan <cyril.connan@gmail.com>
2020-12-20 15:59:50 +0100

    Env vars

--- init_mongo_test.sh
+++ init_mongo_test.sh
@@ -1,9 +1,9 @@
-use ${DB_NAME}
+use "${DB_NAME}"
 db.createUser({
-    user: ${DB_USER},
-    pwd: ${DB_PASS},
+    user: "${DB_USER}",
+    pwd: "${DB_PASS}",
     roles: [{
         role: 'readWrite',
-        db: ${DB_NAME}
+        db: "${DB_NAME}"
     }]
 })
