16300fc7bb8f6181e606d6787830ba0a16ebeeaa
Cyril connan <cyril.connan@gmail.com>
2020-12-20 16:17:09 +0100

    Env vars

---
+++ init_mongo_test.js
@@ -0,0 +1,12 @@
+db = db.getSiblingDB(process.en.DB_NAME)
+db.createUser(
+    {
+        user: process.en.DB_USER,
+        pwd: process.en.DB_PASS,
+        roles: [
+            {
+                role: "readWrite",
+                db: process.en.DB_NAME
+            }
+        ]
+    }
